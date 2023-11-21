48580461248b553b45c737c2aae89f5127454363
brennanleez-coder <brennanlee95@gmail.com>
2023-11-21 17:31:14 +0800

    remove old hashing function

--- app/src/main/java/com/example/apksentinel/utils/HashUtil.kt
+++ app/src/main/java/com/example/apksentinel/utils/HashUtil.kt
@@ -7,16 +7,6 @@ import java.security.MessageDigest
 
 object HashUtil {
 
-    fun getSHA256HashOfFile(filePath: String): String {
-        val digest = MessageDigest.getInstance("SHA-256")
-        val fileBytes = filePath.toByteArray()
-        val bytes = digest.digest(fileBytes)
-        val stringBuilder = StringBuilder()
-        for (byte in bytes) {
-            stringBuilder.append(String.format("%02X", byte))
-        }
-        return stringBuilder.toString()
-    }
 
     fun hashApk(apkFilePath: String, algorithm: String): String {
         val file = File(apkFilePath)
